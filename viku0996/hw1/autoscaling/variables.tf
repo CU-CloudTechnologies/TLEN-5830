88c32d1c0e7d7a33511bb35f167f7ffce35ba826
vibhav-k <vibhav.kurtarkar@colorado.edu>
2018-03-02 03:01:41 -0700

    Changes to readme, add comments, add environment variables support

--- viku0996/hw1/autoscaling/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "ami" {}
-variable "key-name" {}
-variable "security-group-webserver" {}
-variable "dbname" {}
-variable "elb" {}
-variable "public-subnet" {}
--- viku0996/hw1/network/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "vpc-cidr" {}
-variable "private-subnet-cidr" {}
-variable "public-subnet-cidr" {}
